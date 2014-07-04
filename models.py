937c4b6857ff1dc1351dfb59466bcaefc85558a2
Junaid Babu <junaidvayoli@gmail.com>
2014-07-04 09:30:22 +0900

    id had to be uneque

--- models.py
+++ models.py
@@ -5,5 +5,5 @@ from flasky import db
 class Video(db.Document):
     created_at = db.DateTimeField(default=datetime.datetime.now, required=True)
     title = db.StringField(max_length=255, required=True)
-    vid = db.StringField(max_length=255, required=True)
+    vid = db.StringField(max_length=255, required=True, unique=True)
     url = db.StringField(required=False)
